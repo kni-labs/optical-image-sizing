53938b5802ddaff2c040d9d5556b1635b63f3242
Cooper Floyd <cooperwfloyd@gmail.com>
2025-07-11 11:59:37 -0500

    adjust vite config to mirror src structure + move typescript types output to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,53 @@
+import type { Plugin } from 'vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import { globSync } from 'glob';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  build: {
-    outDir: '../dist',
-    rollupOptions: {},
+const copyTypesPlugin = (): Plugin => ({
+  name: 'copy-types',
+  generateBundle(): void {
+    const typeFiles = globSync('**/*.d.ts', {
+      cwd: 'src',
+    });
+
+    typeFiles.forEach((file: string) => {
+      const source = path.resolve('src', file);
+      const content = fs.readFileSync(source);
+
+      this.emitFile({
+        type: 'asset',
+        fileName: file,
+        source: content,
+      });
+    });
   },
-  plugins: [react()],
+});
+
+const config = defineConfig({
+  build: {
+    emptyOutDir: true,
+    outDir: '../dist',
+    rollupOptions: {
+      input: Object.fromEntries(
+        globSync('**/*.{html,ts,tsx}', {
+          cwd: 'src',
+          ignore: ['**/*.d.ts'],
+        }).map((file: string) => [
+          file.replace(/\.(html|ts|tsx)$/, ''),
+          path.resolve('src', file),
+        ]),
+      ),
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+  plugins: [react(), copyTypesPlugin()],
   root: 'src',
 });
+
+export default config;
