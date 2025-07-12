1de7c64c831f6469b2c0b4a10431dababba363e1
Cooper Floyd <cooperwfloyd@gmail.com>
2025-07-12 00:58:48 -0500

    allow for ref forwarding and incoming props

--- src/react/components/optically-sized-image/index.tsx
+++ src/react/components/optically-sized-image/index.tsx
@@ -1,35 +1,60 @@
 import React from 'react';
 import useOpticalImageSize from '../../hooks/use-optical-image-size';
 
+// TODO: move types to library
+
 type OpticallySizedImageProps<ComponentType extends React.ElementType = 'img'> =
-  Omit<React.ComponentPropsWithoutRef<ComponentType>, 'ref' | 'src'> & {
-    src: string;
+  Omit<React.ComponentPropsWithoutRef<ComponentType>, 'ref'> & {
     component?: ComponentType;
   };
 
-const OpticallySizedImage = <ComponentType extends React.ElementType = 'img'>({
-  src,
-  component,
-  ...otherProps
-}: OpticallySizedImageProps<ComponentType>): React.JSX.Element => {
-  const { imgRef, ready, scale } = useOpticalImageSize(src);
-  const ImageComponent = component ?? 'img';
+const OpticallySizedImage = React.forwardRef<
+  HTMLImageElement,
+  OpticallySizedImageProps<React.ElementType>
+>((props, forwardedRef) => {
+  const {
+    src = '',
+    alt = '',
+    component = 'img',
+    ...otherProps
+  } = props as OpticallySizedImageProps & {
+    src: string;
+    component?: React.ElementType;
+  };
 
-  return (
-    <ImageComponent
-      ref={imgRef}
-      src={src}
-      alt=""
-      style={
-        {
-          '--optical-image-size': scale,
-          opacity: ready ? 1 : 0,
-          transition: 'opacity 0.2s ease',
-        } as React.CSSProperties
+  const { imgRef, ready, scale } = useOpticalImageSize(src);
+
+  const combinedRef = React.useCallback(
+    (element: HTMLImageElement | null) => {
+      if (imgRef.current !== element) {
+        imgRef.current = element;
       }
-      {...otherProps}
-    />
+
+      if (typeof forwardedRef === 'function') {
+        forwardedRef(element);
+      } else if (forwardedRef && typeof forwardedRef === 'object') {
+        forwardedRef.current = element;
+      }
+    },
+    [imgRef, forwardedRef],
   );
-};
+
+  const elementProps = {
+    ref: combinedRef,
+    src,
+    alt,
+    ...otherProps,
+    style: {
+      '--optical-image-size': scale,
+      opacity: ready ? 1 : 0,
+      transition: 'opacity 0.2s ease',
+      ...props.style,
+    } as React.CSSProperties,
+  };
+
+  return React.createElement(component, elementProps);
+});
+
+OpticallySizedImage.displayName = 'OpticallySizedImage';
 
 export default OpticallySizedImage;
